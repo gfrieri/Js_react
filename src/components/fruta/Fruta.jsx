2e40179e54a3dde02411a5d8dab474e89f3f1c5f
Giuliano Frieri <gfrieri@uninorte.edu.co>
2023-10-31 23:10:33 -0500

    Update Fruta.jsx

--- src/components/fruta/Fruta.jsx
+++ src/components/fruta/Fruta.jsx
@@ -1,39 +1,44 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Style from "./Fruta.module.css";
+import Datacont from "../../dataContext/dataContext";
 
 const Fruta = ({ name, price, bus }) => {
-    const [cantidad, setCantidad] = useState(0);
+  const [cantidad, setCantidad] = useState(0);
+  const data = useContext(Datacont);
 
-    const agregar = () => {
-        setCantidad(cantidadActual => cantidadActual + 1);
-        bus ({
-            price,
-            operation: true
-        });
-    };
+  const agregar = () => {
+    setCantidad((cantidadActual) => cantidadActual + 1);
+    bus({
+      price,
+      operation: true,
+    });
+  };
 
-    const quitar = () => {
-        setCantidad(cantidadActual => cantidadActual - 1);
-        bus ({
-            price,
-            operation: false
-        });
-    };
+  const quitar = () => {
+    setCantidad((cantidadActual) => cantidadActual - 1);
+    bus({
+      price,
+      operation: false,
+    });
+  };
 
-    return (
-        <div className={Style.fruta}>
-            <h2>Nombre: {name}</h2>
-            <h3>Precio: ${price}</h3>
+  return (
+    <div className={Style.fruta}>
+      <h2>Nombre: {name}</h2>
+      <h3>Precio: ${price}</h3>
 
-            <button onClick={agregar}>+</button>
-            <button onClick={quitar} disabled={cantidad === 0}>-</button>
+      <button onClick={agregar}>+</button>
+      <button onClick={quitar} disabled={cantidad === 0}>
+        -
+      </button>
 
-            <p>Cantidad: {cantidad}</p>
-            <p>Subtotal: {price * cantidad}</p>
+      <p>Cantidad: {cantidad}</p>
+      <p>Subtotal: {price * cantidad}</p>
+      <h3>total: {data}</h3>
 
-            <hr />
-        </div>
-    );
+      <hr />
+    </div>
+  );
 };
 
 export default Fruta;
